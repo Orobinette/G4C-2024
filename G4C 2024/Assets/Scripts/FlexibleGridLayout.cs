dff60f99518aaea4715746d5c85eb3ea49f1cd45
Orsonic13 <robinetteorson@gmail.com>
2023-12-09 15:23:42 -0800

    Seperated vertical and horizontal calculations

--- G4C 2024/Assets/Scripts/FlexibleGridLayout.cs
+++ G4C 2024/Assets/Scripts/FlexibleGridLayout.cs
@@ -8,8 +8,8 @@ public class FlexibleGridLayout : LayoutGroup
     public enum FitType
     {
         Uniform,
-        Width,
-        Height,
+        PrioritizeWidth,
+        PrioritizeHeight,
         FixedRows,
         FixedColumns
     }
@@ -18,71 +18,94 @@ public class FlexibleGridLayout : LayoutGroup
     public int rows;
     public int columns;
 
-    public Vector2 cellSize;
     public Vector2 spacing;
+    private Vector2 cellSize;
+    private Vector2 anchorPoint;
 
     public override void CalculateLayoutInputHorizontal()
     {
         base.CalculateLayoutInputHorizontal();
 
+        //Grid dimensions
         float sqrt = Mathf.Sqrt(transform.childCount);
-
         switch(fitType)
         {
             case FitType.Uniform:
                 columns = Mathf.CeilToInt(sqrt);
-                rows = Mathf.CeilToInt(sqrt);
                 break;
 
-            case FitType.Width:
+            case FitType.PrioritizeWidth:
                 columns = Mathf.CeilToInt(sqrt);
-                rows = Mathf.CeilToInt(rectChildren.Count / (float) columns);
                 break;
 
-            case FitType.Height:
-                rows = Mathf.CeilToInt(sqrt);
-                columns = Mathf.CeilToInt(rectChildren.Count / (float) rows);
+            case FitType.PrioritizeHeight:
+                columns = Mathf.CeilToInt(rectChildren.Count / (float) Mathf.CeilToInt(sqrt));
                 break;
 
             case FitType.FixedRows:
                 columns = Mathf.CeilToInt(rectChildren.Count / (float) rows);
+                break; 
+        }
+
+        //Cell dimensions
+        float parentWidth = rectTransform.rect.width;
+        cellSize.x = (parentWidth - (padding.left + padding.right) - spacing.x * (columns - 1)) / (float) columns;
+    }
+    public override void CalculateLayoutInputVertical()
+    {
+        //Grid dimensions
+        float sqrt = Mathf.Sqrt(transform.childCount);
+        switch(fitType)
+        {
+            case FitType.Uniform:
+                rows = Mathf.CeilToInt(sqrt);   
                 break;
-            
+
+            case FitType.PrioritizeWidth:
+                rows = Mathf.CeilToInt(rectChildren.Count / (float) Mathf.CeilToInt(sqrt));
+                break;
+
+            case FitType.PrioritizeHeight:
+                rows = Mathf.CeilToInt(sqrt);
+                break;
+
             case FitType.FixedColumns:
                 rows = Mathf.CeilToInt(rectChildren.Count / (float) columns);
                 break;  
         }
 
-        float parentWidth = rectTransform.rect.width;
         float parentHeight = rectTransform.rect.height;
-
-        float cellWidth = (parentWidth - (padding.left + padding.right) - spacing.x * (columns - 1)) / (float) columns;
-        float cellHeight = (parentHeight - (padding.top + padding.bottom) - spacing.y * (rows - 1)) / (float) rows;
-
-        cellSize.x = cellWidth;
-        cellSize.y = cellHeight;
+        cellSize.y = (parentHeight - (padding.top + padding.bottom) - spacing.y * (rows - 1)) / (float) rows;
     }
-    public override void CalculateLayoutInputVertical(){}
 
     public override void SetLayoutHorizontal()
     {
         int columnCount = 0;
+
+        for(int i = 0; i < rectChildren.Count; i++)
+        {
+            var tile = rectChildren[i];
+
+            columnCount = i % columns;
+
+            float xPos = ((cellSize.x + spacing.x) * columnCount + padding.left);
+            SetChildAlongAxis(tile, 0, xPos, cellSize.x);
+        }
+    }
+
+    public override void SetLayoutVertical()
+    {
         int rowCount = 0;
 
         for(int i = 0; i < rectChildren.Count; i++)
         {
-            rowCount = i / columns;
-            columnCount = i % columns;
-
             var tile = rectChildren[i];
 
-            var xPos = ((cellSize.x + spacing.x) * columnCount + padding.left);
-            var yPos = ((cellSize.y + spacing.y) * rowCount + padding.top);
+            rowCount = i / columns;
 
-            SetChildAlongAxis(tile, 0, xPos, cellSize.x);
+            float yPos = ((cellSize.y + spacing.y) * rowCount + padding.top);
             SetChildAlongAxis(tile, 1, yPos, cellSize.y);
         }
-    }
 
-    public override void SetLayoutVertical(){}
+    }
 }
